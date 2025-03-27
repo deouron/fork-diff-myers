147050bd7238d3bda43fe462a25582c4ec8bd9f5
deouron <dima.sidorov.2003@list.ru>
2025-03-27 12:16:36 +0300

    deleted usage comment

--- main.cpp
+++ main.cpp
@@ -68,8 +68,3 @@ int main(int argc, char* argv[]) {
     
     return 0;
 }
-
-// сборка: g++ -std=c++17 -o diff_app main.cpp UniversalTokenizer.cpp MyersDiff.cpp
-// применение: ./diff_app old.txt new.txt , либо ./diff_app 
-
-// тесты g++ -std=c++17 -o run_tests tests.cpp UniversalTokenizer.cpp MyersDiff.cpp ./run_tests
